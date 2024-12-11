fc6c5a39d757f753f94b3e4ee55e350e2405c746
Mihai Rusu <rusumiihai@gmail.com>
2024-12-11 14:06:39 +0200

    made some work on transaction and account stuff

--- src/main/java/md/bank/onlinebank/service/impl/TransactionServiceImpl.java
+++ src/main/java/md/bank/onlinebank/service/impl/TransactionServiceImpl.java
@@ -18,7 +18,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
